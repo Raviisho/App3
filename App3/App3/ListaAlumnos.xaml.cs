29bf1d6b4abcef50ff56084a71b37b854ee1fcef
Marcos Lescano <marcoslescano02@gmail.com>
2022-05-27 20:08:38 -0300

    ListaAlumnosOriginal

--- App3/App3/ListaAlumnos.xaml.cs
+++ App3/App3/ListaAlumnos.xaml.cs
@@ -15,56 +15,56 @@ namespace App3
         public ListaAlumnos()
         {
             InitializeComponent();
+            btnVentanaEmergente.Clicked += MostrarVentana;
         }
 
-        private void DarkMode(object sender, EventArgs args)
+        private void MostrarVentana(object sender, EventArgs e)
+        {
+            DisplayAlert("Mensaje", "Hacer la tarea", "OK");
+        }
+
+        private void SetDarkMode(object sender, EventArgs args)
         {
 
             if (btnDarkMode.Text == "DarkMode")
             {
                 btnDarkMode.Text = "WhiteMode";
-                btnDarkMode.TextColor = Color.Azure;
-                btnDarkMode.BackgroundColor = Color.FromHex("#2f2f2f");
-
-                lblMarcos.TextColor = Color.White;
-                lblTomas.TextColor = Color.White;
-                lblLautaro.TextColor = Color.White;
-                lblChino.TextColor = Color.White;
-                lblGaspar.TextColor = Color.White;
-                lblAlan.TextColor = Color.White;
-                lblCesar.TextColor = Color.White;
-                lblJoana.TextColor = Color.White;
-                lblGabriel.TextColor = Color.White;
-
-                stkBody.BackgroundColor = Color.FromHex("#2f2f2f");
-
-                frmFondo.BackgroundColor = Color.FromHex("#1f1f1f");
-
-                lblTitulo.TextColor = Color.Azure;
+                SetColorDarkMode(Color.White, Color.FromHex("#1f1f1f"), Color.Azure, Color.FromHex("#2f2f2f"));
+                
             }
             else
             {
+
                 btnDarkMode.Text = "DarkMode";
-                btnDarkMode.TextColor = Color.Black;
-                btnDarkMode.BackgroundColor = Color.FromHex("#efefef");
+                SetColorDarkMode(Color.Black, Color.White, Color.FromHex("#1f1f1f"), Color.FromHex("#efefef"));
 
-                lblMarcos.TextColor = Color.FromHex("#1f1f1f");
-                lblTomas.TextColor = Color.FromHex("#1f1f1f");
-                lblLautaro.TextColor = Color.FromHex("#1f1f1f");
-                lblChino.TextColor = Color.FromHex("#1f1f1f");
-                lblGaspar.TextColor = Color.FromHex("#1f1f1f");
-                lblAlan.TextColor = Color.FromHex("#1f1f1f");
-                lblCesar.TextColor = Color.FromHex("#1f1f1f");
-                lblJoana.TextColor = Color.FromHex("#1f1f1f");
-                lblGabriel.TextColor = Color.FromHex("#1f1f1f");
-
-                stkBody.BackgroundColor = Color.FromHex("#efefef");
-
-                frmFondo.BackgroundColor = Color.White;
-
-                lblTitulo.TextColor = Color.Black;
             }
 
+        }
+
+        private void SetColorDarkMode(Color ColorTextPrimary, Color ColorBackgroundPrimary, Color ColorTextSecondary, Color ColorBackgroundSecondary)
+        {
+            
+            btnDarkMode.TextColor = ColorTextPrimary;
+            lblTitulo.TextColor = ColorTextPrimary;
+
+            lblMarcos.TextColor = ColorTextSecondary;
+            lblTomas.TextColor = ColorTextSecondary;
+            lblLautaro.TextColor = ColorTextSecondary;
+            lblChino.TextColor = ColorTextSecondary;
+            lblGaspar.TextColor = ColorTextSecondary;
+            lblAlan.TextColor = ColorTextSecondary;
+            lblCesar.TextColor = ColorTextSecondary;
+            lblJoana.TextColor = ColorTextSecondary;
+            lblGabriel.TextColor = ColorTextSecondary;
+
+            btnDarkMode.BackgroundColor = ColorBackgroundSecondary;
+            stkBody.BackgroundColor = ColorBackgroundSecondary;
+            stkBody2.BackgroundColor = ColorBackgroundSecondary;
+
+            frmFondo.BackgroundColor = ColorBackgroundPrimary;
+
+            
         }
     }
 }
